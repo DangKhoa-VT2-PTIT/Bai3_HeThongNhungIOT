07a40ab7dbe9b57879f5bf5c320090325a13c01e
khoaND <dangkhoa201102@gmail.com>
2024-03-29 13:49:05 +0700

    first commit

---
+++ Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ D:/Dai_Hoc/Ki_2_Nam_3/THUC_TAP_CO_SO/CubeIDE/SDK_1.0.3_NUCLEO-F401RE/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rcc.h \
+ D:/Dai_Hoc/Ki_2_Nam_3/THUC_TAP_CO_SO/CubeIDE/SDK_1.0.3_NUCLEO-F401RE/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h \
+ D:/Dai_Hoc/Ki_2_Nam_3/THUC_TAP_CO_SO/CubeIDE/SDK_1.0.3_NUCLEO-F401RE/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h \
+ D:/Dai_Hoc/Ki_2_Nam_3/THUC_TAP_CO_SO/CubeIDE/SDK_1.0.3_NUCLEO-F401RE/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h \
+ D:/Dai_Hoc/Ki_2_Nam_3/THUC_TAP_CO_SO/CubeIDE/SDK_1.0.3_NUCLEO-F401RE/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h \
+ D:/Dai_Hoc/Ki_2_Nam_3/THUC_TAP_CO_SO/CubeIDE/SDK_1.0.3_NUCLEO-F401RE/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h \
+ D:/Dai_Hoc/Ki_2_Nam_3/THUC_TAP_CO_SO/CubeIDE/SDK_1.0.3_NUCLEO-F401RE/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h \
+ D:/Dai_Hoc/Ki_2_Nam_3/THUC_TAP_CO_SO/CubeIDE/SDK_1.0.3_NUCLEO-F401RE/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_gpio.h
+D:/Dai_Hoc/Ki_2_Nam_3/THUC_TAP_CO_SO/CubeIDE/SDK_1.0.3_NUCLEO-F401RE/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rcc.h:
+D:/Dai_Hoc/Ki_2_Nam_3/THUC_TAP_CO_SO/CubeIDE/SDK_1.0.3_NUCLEO-F401RE/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h:
+D:/Dai_Hoc/Ki_2_Nam_3/THUC_TAP_CO_SO/CubeIDE/SDK_1.0.3_NUCLEO-F401RE/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h:
+D:/Dai_Hoc/Ki_2_Nam_3/THUC_TAP_CO_SO/CubeIDE/SDK_1.0.3_NUCLEO-F401RE/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h:
+D:/Dai_Hoc/Ki_2_Nam_3/THUC_TAP_CO_SO/CubeIDE/SDK_1.0.3_NUCLEO-F401RE/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h:
+D:/Dai_Hoc/Ki_2_Nam_3/THUC_TAP_CO_SO/CubeIDE/SDK_1.0.3_NUCLEO-F401RE/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h:
+D:/Dai_Hoc/Ki_2_Nam_3/THUC_TAP_CO_SO/CubeIDE/SDK_1.0.3_NUCLEO-F401RE/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h:
+D:/Dai_Hoc/Ki_2_Nam_3/THUC_TAP_CO_SO/CubeIDE/SDK_1.0.3_NUCLEO-F401RE/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_gpio.h:
